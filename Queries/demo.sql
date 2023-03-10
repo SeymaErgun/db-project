1c7382c3b390eb6fc43a6eac6bd0622c40960757
seymaergun <seymaergun89@gmail.com>
2023-03-10 15:24:50 +0100

    Initial commit

---
+++ Queries/demo.sql
@@ -0,0 +1,28 @@
+select * from EMPLOYEES ;
+--reads all column from employees table
+
+select * from DEPARTMENTS;
+--reads all column from departments table
+
+select first_name from EMPLOYEES;
+--get only firstname from employees table
+
+select city from LOCATIONS;
+--display city names
+
+select first_name, last_name, salary from EMPLOYEES;
+--get me first_name, last_name and salary
+
+select STREET_ADDRESS, POSTAL_CODE from LOCATIONS;
+--get me street adress and postal code
+
+select distinct first_name from EMPLOYEES;
+--remove duplicates
+
+
+
+
+
+
+
+
---
+++ Queries/demo2.sql
@@ -0,0 +1,26 @@
+--I want to see firstname lastname phone number of david(s)
+
+select first_name, last_name, PHONE_NUMBER from EMPLOYEES where FIRST_NAME = 'David' and LAST_NAME ='Lee';
+
+--get me all information who is making more than 7000 salary
+select * from EMPLOYEES where SALARY > 7000;
+
+--get me email of who is making less than 4000
+select EMAIL, SALARY from EMPLOYEES
+where SALARY < 4000;
+
+select * from DEPARTMENTS ;
+
+--get me all info who is working as IT_PROG or SA_REP
+select * from EMPLOYEES
+where JOB_ID = 'IT_PROG' or JOB_ID = 'SA_REP';
+
+--get me first_name, last_name salary who is making more than 5000 and less than 10000
+
+select FIRST_NAME, LAST_NAME, SALARY
+from EMPLOYEES
+where  SALARY >= 5000 and SALARY <= 10000;
+
+select FIRST_NAME, LAST_NAME, SALARY
+from EMPLOYEES
+where  SALARY between 5000 and 10000;
