1d10f23b6377be890e0d19d12fe2f7c57694648e
seymaergun <seymaergun89@gmail.com>
2023-03-10 20:09:13 +0100

    HR4 is added

--- Queries/HR4.sql
+++ Queries/HR4.sql
@@ -14,4 +14,8 @@ from EMPLOYEES
 group by JOB_ID;
 
 
-select * from EMPLOYEES;
+--get me job_ids where their avg salary is more than 5k
+select JOB_ID, avg(salary), count(*), sum(SALARY)
+from EMPLOYEES
+group by JOB_ID
+having avg(salary)>5000;
