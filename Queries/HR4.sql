7e6f7c50b96f02792df56f6fdc4b0e9d5139bcd4
seymaergun <seymaergun89@gmail.com>
2023-03-10 19:40:30 +0100

    HR4 is added

--- Queries/HR4.sql
+++ Queries/HR4.sql
@@ -1,4 +1,17 @@
+
+--get me unique job_ids
+select distinct JOB_ID from EMPLOYEES;
+
+
+
 --get me average salary for IT_PROG
-select *
+select AVG(SALARY)
 from EMPLOYEES
-where  ;
+where JOB_ID ='IT_PROG';
+
+select JOB_ID, avg(salary), count(*), sum(SALARY)
+from EMPLOYEES
+group by JOB_ID;
+
+
+select * from EMPLOYEES;
