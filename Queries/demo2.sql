8be3982392e6c9d3f63b753d8a4ebe8ed1aa2dfb
seymaergun <seymaergun89@gmail.com>
2023-03-10 16:22:27 +0100

    sth is added

--- Queries/demo2.sql
+++ Queries/demo2.sql
@@ -24,3 +24,26 @@ where  SALARY >= 5000 and SALARY <= 10000;
 select FIRST_NAME, LAST_NAME, SALARY
 from EMPLOYEES
 where  SALARY between 5000 and 10000;
+
+select *
+from EMPLOYEES
+where EMPLOYEE_ID between 130 and 170;
+
+--get me all info where employee_id 135 176 154 129
+select *
+from EMPLOYEES
+where EMPLOYEE_ID=135 or EMPLOYEE_ID= 176 or EMPLOYEE_ID=154 or EMPLOYEE_ID=129 ;
+
+select *
+from EMPLOYEES
+where EMPLOYEE_ID in(135, 176, 154, 129);
+
+--get me city of where country_id IT,US,UK
+select city, COUNTRY_ID from LOCATIONS
+where COUNTRY_ID in ('IT','US','UK');
+
+
+
+
+
+
